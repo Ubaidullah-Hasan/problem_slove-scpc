bfb3b0d6c657b987b84269d31c9d8869cb77f3e1
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-07-25 21:05:16 +0600

    task2

---
+++ task2.js
@@ -0,0 +1,12 @@
+const positiveNumber = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) { 
+        if(arr[i] > 0){
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+const numbers = [2, -5, 10, -3, 7]
+const result = positiveNumber(numbers);
+console.log(result);
